92525ac4522cb31e0aee35242f14db9370fc16a1
hyunbenny <bluesky3268@naver.com>
2024-03-20 22:35:40 +0900

    jekyll serve 안되는 부분 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710941201',resources: [
+    cacheName: 'chirpy-1710941688',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
